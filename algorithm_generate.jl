e0ee5d3c383075d60607e875205f0dd281e83ff3
AdrienBlassiau <adblassiau@gmail.com>
2019-12-06 11:48:00 +0100

    feat(test): generated test

--- algorithm_generate.jl
+++ algorithm_generate.jl
@@ -3,6 +3,6 @@ include("./helpers/generate.jl")
 
 using.Generator
 
-for i in 2:4
-	generate(i,i,2,0.5,0.5,(2,3),(2,3),"test_wh$i.input")
+for i in 2:5
+	generate(i,i,2,0.5,0.5,(2,3),(2,3),"generated_test/test_wh$i.input")
 end
